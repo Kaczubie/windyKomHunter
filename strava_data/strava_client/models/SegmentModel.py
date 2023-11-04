8d66ad9b3d176931cd1d3ccd021ce8fd29befc02
Kacper-Zubiel-airspace-intelligence <kacper.zubiel@airspace-intelligence.com>
2023-11-04 22:49:08 +0100

    initial commit

---
+++ strava_data/strava_client/models/SegmentModel.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class SegmentModel(BaseModel):
+    id: int
+    name: str
+    distance: float
+    start_latlng: list[float]
+    end_latlng: list[float]
+    city: str | None = None
+    pr_time: int | None = None
+
+
+
